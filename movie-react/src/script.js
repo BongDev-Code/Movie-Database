b7e7f458b3dedcc6817f7fbf7ff36e6fbd638054
Bongani Nyongwane <nyongwanebongani@gmail.com>
2025-04-08 14:12:57 +0200

    Initial commit for a Movie Website

---
+++ movie-react/src/script.js
@@ -0,0 +1,25 @@
+const API_KEY = "2c51a409-dee4-4fb1-8c0a-fe6392c96833";
+
+//Popular movie titles to display by default
+const popularMovies = [
+    'The Shawshank Redemption',
+    'The Godfather',
+    'The Dark Knight',
+    'Pulp Fiction',
+    'Forrest Gump',
+    'Inception',
+    'Fight Club',
+    'The Matrix',
+    'The Lord of the Rings: The Return of the King',
+    'Interstellar',
+    'The Empire Strikes Back',
+    'The Avengers',
+    'Gladiator',
+    'Titanic',
+    'Jurassic Park',
+    'The Silence of the Lambs',
+];
+
+
+//DOM ELEMENTS
+const searchInput = document.getElementById('searchInput');
